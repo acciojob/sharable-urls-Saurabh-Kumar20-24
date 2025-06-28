4213ebbdd33ed1bacf9f5e388be70e23a65059e5
Saurabh Kumar <146050167+Saurabh-Kumar20-24@users.noreply.github.com>
2025-06-28 14:54:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // your code here
+const name= document.getElementById("name")
+const year= document.getElementById("year")
+const button= document.getElementById("button")
+const url= document.getElementById("url")
+
+
+button.addEventListener("click",(e)=>{
+	e.preventDefault()
+// alert(url.textContent+ name.value+ year.value)
+	
+	url.textContent = `${url.textContent}?name=${name.value}&year=${year.value}`
+})
